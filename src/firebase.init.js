6b5de49b4660428f214a7980b1d6243cd7508aa7
ImAlAmin <alamin.cse97@gmail.com>
2022-09-04 12:35:41 +0600

    Login form field validation using regular expression, show error

---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZK008HZYN3tnqIuGfu7W6zvCxDqBrUyY",
+  authDomain: "email-password-auth-b3dd5.firebaseapp.com",
+  projectId: "email-password-auth-b3dd5",
+  storageBucket: "email-password-auth-b3dd5.appspot.com",
+  messagingSenderId: "953231842769",
+  appId: "1:953231842769:web:8129b1c8493da16bec54f0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
